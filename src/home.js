8c547f025202761cf89ca354b88dd479c093e1ad
Jakob <128335378+jakobm50@users.noreply.github.com>
2023-09-23 18:11:33 -0500

    likes list

--- src/home.js
+++ src/home.js
@@ -1,41 +1,49 @@
 //creating liked and disliked lists
-let likedList = [];
-let dislikedList = [];
+let likedRestaurants = [];
+let dislikedRestaurants = [];
 
 //function to update the liked list on the webpage
-function updateLikedList() {
-  const likedListElement = document.getElementById('likedList');
-  likedListElement.innerHTML = '';
+function updateLikedRestaurants() {
+  const likedRestaurantsList = document.getElementById('likedRestaurants');
+  likedRestaurantsList.innerHTML = '';
 
-  for (const item of likedList) {
+  for (const restaurant of likedRestaurants) {
     const li = document.createElement('li');
-    li.textContent = item;
-    likedListElement.appendChild(li);
+    li.textContent = restaurant;
+    likedRestaurantsList.appendChild(li);
   }
 }
 
 //function to update the disliked list on the webpage
-function updateDislikedList() {
-  const dislikedListElement = document.getElementById('dislikedList');
-  dislikedListElement.innerHTML = '';
+function updateDislikedRestaurants() {
+  const dislikedRestaurantsList = document.getElementById('dislikedRestaurants');
+  dislikedRestaurantsList.innerHTML = '';
 
-  for (const item of dislikedList) {
+  for (const restaurant of dislikedRestaurants) {
     const li = document.createElement('li');
-    li.textContent = item;
-    dislikedListElement.appendChild(li);
+    li.textContent = restaurant;
+    dislikedRestaurantsList.appendChild(li);
   }
 }
 
 //
-const likeButton = document.getElementById('likeButton');
-const dislikeButton = document.getElementById('dislikeButton');
+const likeButtons = document.querySelectorAll('.likeButton');
+const dislikeButtons = document.querySelectorAll('.dislikeButton');
 
-likeButton.addEventListener('click', () => {
-  likedList.push(document.getElementById('restaurantName'));
-  updateLikedList();
+likeButtons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    const restaurantName = document.querySelectorAll('.restaurantName')
+    [index].textContent;
+      likedRestaurants.push(restaurantName);
+      updateLikedRestaurants();
+  });
 });
 
-dislikeButton.addEventListener('click', () => {
-  dislikedList.push(document.getElementById('restaurantName'));
-  updateDislikedList();
+dislikeButtons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    const restaurantName = document.querySelectorAll('.restaurantName')
+    [index].textContent;
+  dislikedRestaurants.push(restaurantName);
+  updateDislikedRestaurants();
+  });
 });
