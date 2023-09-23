55d2ccbc52a80ab4fc56e7bcb370ce99acd6606d
Jakob <128335378+jakobm50@users.noreply.github.com>
2023-09-23 16:33:38 -0500

    likes

--- src/home.js
+++ src/home.js
@@ -1,21 +1,41 @@
-var likeBtn = document.getElementById('green');
-var dislikeBtn = document.getElementById('red');
+//creating liked and disliked lists
+let likedList = [];
+let dislikedList = [];
 
-likeBtn.addEventListener('click', function() {
-  
-    if (dislikeBtn.classList.contains('red')) {
-      dislikeBtn.classList.remove('red');
-    } 
-  this.classList.toggle('green');
-  
+//function to update the liked list on the webpage
+function updateLikedList() {
+  const likedListElement = document.getElementById('likedList');
+  likedListElement.innerHTML = '';
+
+  for (const item of likedList) {
+    const li = document.createElement('li');
+    li.textContent = item;
+    likedListElement.appendChild(li);
+  }
+}
+
+//function to update the disliked list on the webpage
+function updateDislikedList() {
+  const dislikedListElement = document.getElementById('dislikedList');
+  dislikedListElement.innerHTML = '';
+
+  for (const item of dislikedList) {
+    const li = document.createElement('li');
+    li.textContent = item;
+    dislikedListElement.appendChild(li);
+  }
+}
+
+//
+const likeButton = document.getElementById('likeButton');
+const dislikeButton = document.getElementById('dislikeButton');
+
+likeButton.addEventListener('click', () => {
+  likedList.push(restaurant);
+  updateLikedList();
 });
 
-dislikeBtn.addEventListener('click', function() {
-  
-    if (likeBtn.classList.contains('green')) {
-      likeBtn.classList.remove('green');
-    } 
-  this.classList.toggle('red');
-  
+dislikeButton.addEventListener('click', () => {
+  dislikedList.push(restaurant);
+  updateDislikedList();
 });
-
