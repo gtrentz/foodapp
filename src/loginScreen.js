ec7e81f4f4f47371e4c14e5f066cd41679ae612d
gtrentz <grant-trentz@uiowa.edu>
2023-09-23 10:43:40 -0500

    Some classes added

--- src/loginScreen.js
+++ src/loginScreen.js
@@ -3,5 +3,22 @@ class User {
         this.name = name;
         this.user = username;
         this.pass = password;
+        this.liked = [];
         }
+}
+
+class Restaurant {
+    constructor(name, rating, image, type) {
+        this.name = name;
+        this.rating = rating;
+        this.image = image;
+        this.cuisine = type;
+    }
+}
+
+class Group {
+    constructor(users, restaurants) {
+        this.users = users;
+        this.restaurants = restaurants;
+    }
 }
