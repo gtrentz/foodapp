7246506a3fec13fd22fae9990543ea1543033566
gtrentz <grant-trentz@uiowa.edu>
2023-09-24 12:04:11 -0500

    Current user

--- src/loginCheck.php
+++ src/loginCheck.php
@@ -2,6 +2,8 @@
 $username = $_POST['username'];
 $password = $_POST['password'];
 
+$valid = false;
+
 // Read the users.txt file and split each line into an array
 $usersFile = 'C:\\xampp\\htdocs\\users.txt'; // Your file path
 $userlines = file($usersFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
@@ -12,15 +14,30 @@ foreach ($userlines as $line) {
     if ($username === $storedUsername) {
         // Use password_verify to check if the entered password matches the stored hash
         if (password_verify($password, $storedHashedPassword)) {
-            // Successful login, redirect to a welcome page or perform necessary actions
+            $valid = true;
             echo "Login successful";
             echo "<a href='http://127.0.0.1:5500/html/userhome.html'>Home page</a>";
-            //exit();
-        } else {
+            } else {
             // Invalid login, display an error message or redirect to a login error page
-            echo "Invalid username or password. <a href='http://127.0.0.1:5500/html/login.html'>Try again</a>";
+                echo "Invalid username or password. <a href='http://127.0.0.1:5500/html/login.html'>Try again</a>";
         }
     }
+
+    // if ($valid) {
+    //     $username = $_POST['username']; // Assuming you have already validated the login
+    //         // Create or update currentuser.txt
+    //         $currentuserFile = 'currentuser.txt';
+
+    //         // User information to write (e.g., username)
+    //         $userInfo = "Username: $username\n";
+
+    //         // Write the user information to currentuser.txt
+    //         file_put_contents($currentuserFile, $userInfo);
+
+    //         // Redirect to the user's dashboard or any other page
+    //         header('Location: C:\xampp\htdocs\userhome.html');
+    //         exit; // Terminate the script
+    // }
 }
 
 
