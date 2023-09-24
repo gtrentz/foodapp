1afd32ccc3745e8fc2a046b8837f63ea74c6f45c
gtrentz <grant-trentz@uiowa.edu>
2023-09-24 11:50:30 -0500

    A lot

--- src/create.php
+++ src/create.php
@@ -1,34 +1,34 @@
 <?php
-// Load the existing 'groups' data from the text file
-$filename = "groups.txt";
-$groups = [];
-
-if (file_exists($filename)) {
-    $groups = json_decode(file_get_contents($filename), true);
-}
+// Load the existing 'groups' data from the JSON file
+$jsonFile = 'groups.json';
+$groups = json_decode(file_get_contents($jsonFile), true);
 
 // Get form data
 $groupName = $_POST['group_name'];
 $members = $_POST['members'];
+$restaurants = $_POST['restaurants'];
 $restaurant1 = $_POST['restaurant1'];
 // Extract other restaurant preferences as needed
 
 // Create a new group entry in the 'groups' data structure
 $newGroup = [
     'members' => explode(',', $members),
+    'restaurants' => explode(',', $restaurants),
     'restaurant_preferences' => [
-        'restaurant1' => explode(',', $restaurant1),
-        // Add other restaurant preferences as needed
+        // Initialize preferences for each user here
     ],
 ];
 
 // Update the 'groups' data structure with the new group
 $groups[$groupName] = $newGroup;
 
-// Serialize and save the updated 'groups' data to the text file
-file_put_contents($filename, json_encode($groups));
+// Serialize and save the updated 'groups' data to the JSON file
+file_put_contents($jsonFile, json_encode($groups));
+
+// Save the updated 'groups' data to groups.txt as well
+file_put_contents('groups.txt', json_encode($groups));
 
 // Redirect back to the page or provide feedback
-header('Location: groups.html');
+header('Location: http://127.0.0.1:5500/html/groups.html');
 exit;
 ?>
--- src/loginCheck.php
+++ src/loginCheck.php
@@ -3,26 +3,30 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 
 // Read the users.txt file and split each line into an array
-$userlines = file("users.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-
-// Loop through the lines to find a match
-$validLogin = false;
+$usersFile = 'C:\\xampp\\htdocs\\users.txt'; // Your file path
+$userlines = file($usersFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
 foreach ($userlines as $line) {
-    list($storedUsername, $storedPassword) = explode(':', $line); // Use ':' as delimiter
-    if ($username === $storedUsername && $password === $storedPassword) { // Check for a match
-        $validLogin = true;
-        break; // Match found, exit the loop
+    list($storedUsername, $storedHashedPassword) = explode(':', $line);
+
+    if ($username === $storedUsername) {
+        // Use password_verify to check if the entered password matches the stored hash
+        if (password_verify($password, $storedHashedPassword)) {
+            // Successful login, redirect to a welcome page or perform necessary actions
+            echo "Login successful";
+            echo "<a href='http://127.0.0.1:5500/html/userhome.html'>Home page</a>";
+            //exit();
+        } else {
+            // Invalid login, display an error message or redirect to a login error page
+            echo "Invalid username or password. <a href='http://127.0.0.1:5500/html/login.html'>Try again</a>";
+        }
     }
 }
 
-if ($validLogin) {
-    // Successful login, redirect to a welcome page or perform necessary actions
-    echo "Login successful";
-    echo "<a href='http://127.0.0.1:5500/html/userhome.html'>Home page</a>";
-    //exit();
-} else {
-    // Invalid login, display an error message or redirect to a login error page
-    echo "Invalid username or password. <a href='http://127.0.0.1:5500/html/login.html'>Try again</a>";
-}
+
+
+    
+
+
+    
 ?>
